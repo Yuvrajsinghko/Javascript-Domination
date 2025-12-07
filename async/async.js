b194197e2fff6b236242eac1c39248e967b152b7
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-07 17:45:17 +0530

    added new file which include another concept of  promise

--- async/async.js
+++ async/async.js
@@ -23,8 +23,19 @@ function outerFun() {
   }
   innerFun();
 }
-function getData(dataId) {
+function getData(dataId, getNextData) {
   setTimeout(() => {
     console.log("data", dataId);
-  },2000);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 2000);
 }
+
+getData(1,()=>{
+    getData(2,()=>{
+        getData(3,()=>{
+            getData(4)
+        })
+    })
+})
