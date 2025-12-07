029fd1bd62b9304f731cbc09f28a3532d5636fe3
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-07 13:42:30 +0530

    updated async file with new concepts

--- async/async.js
+++ async/async.js
@@ -1,12 +1,30 @@
-console.log("hii");
-console.log("how are you");
-console.log("Hi bexi");
+// console.log("hii");
+// console.log("how are you");
+// console.log("Hi bexi");
 
-let a = () => {
-  console.log("time out");
-};
+// let a = () => {
+//   console.log("time out");
+// };
 
-setTimeout(a, 2000);
+// setTimeout(a, 2000);
 
-console.log("hoolp");
+// console.log("hoolp");
+// let calculator =
+//   (1,
+//   2,
+//   (a, b) => {
+//     console.log(a + b);
+//   });
 
+// calculator();
+function outerFun() {
+  function innerFun() {
+    console.log("hii");
+  }
+  innerFun();
+}
+function getData(dataId) {
+  setTimeout(() => {
+    console.log("data", dataId);
+  },2000);
+}
