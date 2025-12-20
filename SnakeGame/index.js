87d33b6dbc9d8bbfcbe9befaec78f05ea28310c2
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-20 16:31:01 +0530

    modified styles in snake game project

---
+++ SnakeGame/index.js
@@ -0,0 +1,16 @@
+const board = document.querySelector(".board");
+const blockHeight = 30;
+const blockWidth = 30;
+
+
+
+const cols=Math.floor(board.clientWidth/blockWidth);
+const rows=Math.floor(board.clientHeight/blockHeight);
+console.log(cols,rows);
+
+for (let i = 0; i < rows*cols; i++) {
+   const block=document.createElement('div')
+   block.classList.add('block')
+   board.appendChild(block)
+    
+}
