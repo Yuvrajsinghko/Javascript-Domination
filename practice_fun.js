67a0ebfb5cc8e436f3c2f531e463a70f5c3f46cd
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-01-02 22:30:28 +0530

    added a new js file practice js

---
+++ practice_fun.js
@@ -0,0 +1,44 @@
+// let totalPrize;
+
+// function totalcost(totalPrize){
+
+//     let shippingCost;
+//     //code logic
+
+//     if (totalPrize <=10){
+//         shippingCost =5;
+//     }else if (totalPrize <= 20){
+//         shippingCost = 3;
+//     }else{
+//         shippingCost = 0;
+//     }
+
+//     console.log(`shippingCost for you is  ${shippingCost}`)
+
+// }
+// totalPrize=10;
+
+// totalcost(totalPrize)
+
+// totalPrize=40;
+// totalcost(totalPrize)
+
+// console.log(a);
+// var a = 5;
+
+// function greet() {
+//   console.log("Hello");
+// }
+// greet();
+
+function makeCounter(initialValue = 0) {
+  return function () {
+    return initialValue++;
+  };
+}
+
+let a = makeCounter();
+
+a();
+a();
+a();
