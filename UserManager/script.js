a3640076266e88c9518149c9bde9a8f8db74592e
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-01-03 14:03:15 +0530

    completed userManager project

--- UserManager/script.js
+++ UserManager/script.js
@@ -7,12 +7,12 @@ let photo = document.querySelector("#photo");
 const userManager = {
   users: [],
   init: function () {
-    form.addEventListener("submit", this.submitForm.bind(userManager));
+    form.addEventListener("submit", this.submitForm.bind(this));
+    this.removeUser();
   },
   submitForm: function (e) {
     e.preventDefault();
-    console.log("form submitted");
-    this.addUser()
+    this.addUser();
   },
   addUser: function () {
     this.users.push({
@@ -21,9 +21,62 @@ const userManager = {
       bio: bio.value,
       photo: photo.value,
     });
+
     form.reset();
+    this.renderUi();
   },
-  removeUser: function () {},
+  renderUi: function () {
+    document.querySelector(".users").innerHTML = "";
+    this.users.forEach((user, index) => {
+      const card = document.createElement("div");
+      card.dataset.index = index;
+      card.className =
+        "bg-white/90 backdrop-blur rounded-2xl shadow-xl p-8 flex flex-col items-center border border-blue-100 hover:scale-105 transition";
+
+      // Image
+      const img = document.createElement("img");
+      img.className =
+        "w-28 h-28 rounded-full object-cover mb-5 border-4 border-blue-200 shadow";
+      img.src = user.photo;
+      img.alt = "User Photo";
+      card.appendChild(img);
+
+      // Name
+      const name = document.createElement("h2");
+      name.className = "text-2xl font-bold mb-1 text-blue-700";
+      name.textContent = user.username;
+      card.appendChild(name);
+
+      // Role
+      const role = document.createElement("p");
+      role.className = "text-purple-500 mb-2 font-medium";
+      role.textContent = user.role;
+      card.appendChild(role);
+
+      // Description
+      const desc = document.createElement("p");
+      desc.className = "text-gray-700 text-center";
+      desc.textContent = user.bio;
+      card.appendChild(desc);
+
+      // Finally, append the card wherever needed, for example:
+
+      document.querySelector(".users").appendChild(card);
+    });
+    
+  },
+  removeUser: function () {
+    let req = document.querySelector(".users");
+    req.addEventListener("click", (e) => {
+      const card = e.target.closest("div");
+      if (!card) return;
+
+      const index = Number(card.dataset.index);
+
+      this.users.splice(index,1)
+      this.renderUi()
+    });
+  },
 };
 
 userManager.init();
