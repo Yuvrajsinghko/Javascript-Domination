05277c84fd238bfa6281f0fc79b1c53e4dc1b29f
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-01-25 14:27:06 +0530

    added new folder for DSA in javascript

---
+++ DSA/basics.js
@@ -0,0 +1,15 @@
+// Swapping First Method
+let a = 30;
+let b = 20;
+let c;
+// c = a;
+// a = b;
+// b = c;
+// console.log(a, b);
+
+//Second Method
+
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a, b);
