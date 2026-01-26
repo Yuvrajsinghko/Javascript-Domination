d30f578ff644d6234b5343f31f50d52d6523ae81
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-01-26 22:24:31 +0530

    added new project folder

---
+++ JS_DOM/DOM.js
@@ -0,0 +1,19 @@
+// let para = document.querySelector(".para")
+// let btn =document.querySelector(".btn")
+
+// const handleClick=()=>{
+//     para.textContent="Changed"
+// }
+
+// console.log(para,btn);
+
+// btn.addEventListener("click",handleClick)
+
+let h1=document.querySelector("h1")
+
+h1.classList.toggle("par")
+
+
+
+ 
+
