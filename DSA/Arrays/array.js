a927f06c746ac874df6ebb3c23939479506ab458
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-02-01 18:05:56 +0530

    added solution to problem right rotation by K places

--- DSA/Arrays/array.js
+++ DSA/Arrays/array.js
@@ -118,34 +118,34 @@
 
 // left rotation of  array elements by K times
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8];
-let prompt = require("prompt-sync")();
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let prompt = require("prompt-sync")();
 
-let k = Number(prompt("Enter number to rotate by:"));
-k = Math.floor(k % arr.length);
+// let k = Number(prompt("Enter number to rotate by:"));
+// k = Math.floor(k % arr.length);
 
-for (let t = 1; t <= k; t++) {
-  let cpy = arr[0];
-  for (let i = 0; i < arr.length - 1; i++) {
-    arr[i] = arr[i + 1];
-  }
-  arr[arr.length - 1] = cpy;
-}
+// for (let t = 1; t <= k; t++) {
+//   let cpy = arr[0];
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     arr[i] = arr[i + 1];
+//   }
+//   arr[arr.length - 1] = cpy;
+// }
 
 // Right rotation of array elements by K times
 
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
-// let prompt = require("prompt-sync")();
-// let k = Number(prompt("Enter number to rotate by:"));
-// k = k % arr.length;
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let prompt = require("prompt-sync")();
+let k = Number(prompt("Enter number to rotate by:"));
+k = k % arr.length;
 
-// for (let t = 1; t <= k; t++) {
-//   let cpy = arr[arr.length - 1];
-//   for (let i = arr.length - 1; i > 0; i--) {
-//     arr[i] = arr[i - 1];
-//   }
-//   arr[0] = cpy;
-// }
+for (let t = 1; t <= k; t++) {
+  let cpy = arr[arr.length - 1];
+  for (let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = cpy;
+}
 
 //Efficient  approach to solve rotation ky k times problem
 
