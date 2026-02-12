dc422cb79a87f63744742190e9789231cea19891
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-02-12 09:14:32 +0530

    added new folder to understand the concept of recursion

---
+++ DSA/Recursion/recursion.js
@@ -0,0 +1,15 @@
+greet();
+
+function greet() {
+  let a = 'yuvraj';
+  console.log("Good morning "+a);
+  enjoy();
+}
+
+function enjoy() {
+  console.log("Enjoy your life");
+  temp();
+}
+function temp() {
+  console.log("Kuch kar lo be");
+}
