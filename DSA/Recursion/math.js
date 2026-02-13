854ebda4029e86f15959fd242ec0fc0e86d4e146
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-02-13 16:28:29 +0530

    added program to find factors in an optimized manner

--- DSA/Recursion/math.js
+++ DSA/Recursion/math.js
@@ -33,13 +33,30 @@ function Gcd(a, b) {
 }
 
 //Optmized Approach for GCD of two numbers.
+//time complexity log(max(a,b)).
 
-function gcd(a,b){
-    if (b==0) return a;
-    return gcd(b,a%b)
+function gcd(a, b) {
+  if (b == 0) return a;
+  return gcd(b, a % b);
 }
 
-console.log(gcd(14,16));
+//Factors of Number
+//time complexity square root n.
+let n = 100;
+
+for (let i = 1; i <= Math.floor(Math.sqrt(n)); i++) {
+  if (n % i == 0) {
+    process.stdout.write(i + " ");
+  }
+}
+for (let i = Math.floor(Math.sqrt(n)); i >= 1; i--) {
+  if (n % i == 0) {
+    if (n / i != i) {
+      process.stdout.write(n / i + " ");
+    }
+  }
+}
 
 
+//Count Number of Prime.
 
