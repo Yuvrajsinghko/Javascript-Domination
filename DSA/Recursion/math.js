a5d19f4ad73b7da11fbeff255127837a56fd2445
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-02-13 15:58:24 +0530

    added program to find GCD by iterative and recursive manner

--- DSA/Recursion/math.js
+++ DSA/Recursion/math.js
@@ -1,15 +1,36 @@
 //Find GCD
 
-let a = 20,
-  b = 32,
-  fact = [];
+//Brute Force Method
+// let a = 20,
+//   b = 32,
+//   fact = [];
 
-for (let i = 20; i >= 1; i--) {
-  if (a % i == 0 && b % i == 0) {
-    fact.push(i)
-  }
-}
+// for (let i = 20; i >= 1; i--) {
+//   if (a % i == 0 && b % i == 0) {
+//     fact.push(i)
+//   }
+// }
 // console.log(Math.max(...fact));
 
-//Eucliden 
+//Eucliden Method
 
+//Iterative Method
+
+let a = 20,
+  b = 32;
+
+while (a != b) {
+  if (a > b) a = a - b;
+  else b = b - a;
+}
+
+
+
+//Recursive Way
+function Gcd(a, b) {
+  if (a == b) return a;
+  if (a > b) a = a - b;
+  else b = b - a;
+  return Gcd(a, b);
+}
+console.log(Gcd(1000, 1));
