83ce05a8641e62393c1d7aed267056f3709bb598
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-07 21:39:41 +0530

    updated with some practice examples

---
+++ DSA/Baiscs/basics.js
@@ -0,0 +1,40 @@
+// Swapping First Method
+let a = 30;
+let b = 20;
+let c;
+// c = a;
+// a = b;
+// b = c;
+// console.log(a, b);
+
+//Second Method
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a,b);
+
+//Third Method (Destructuring)
+
+// [a,b]=[b,a]
+// console.log(a,b);
+
+// Operators //
+
+// console.log(Math.floor(42/5));
+
+// console.log(8%24);
+
+//OTP 6 Digit
+
+
+console.log(Math.trunc((Math.random()*10)+1));
+
+
+
+
+
+
+
+
+
