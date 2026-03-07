4cbb24f013ca12cfcd561556e737bbf5b4011b95
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-07 21:38:05 +0530

    updated with promise hell example code

--- Asyncronun-Js/async.js
+++ Asyncronun-Js/async.js
@@ -1,48 +1,48 @@
-// console.log("hii");
-// console.log("how are you");
-// console.log("Hi bexi");
+console.log("hii");
+console.log("how are you");
+console.log("Hi bexi");
 
-// const { log } = require("async");
+const { log } = require("async");
 
-// let a = () => {
-//   console.log("time out");
-// };
+let a = () => {
+  console.log("time out");
+};
 
-// setTimeout(a, 2000);
+setTimeout(a, 2000);
 
-// console.log("hoolp");
-// let calculator =
-//   (1,
-//   2,
-//   (a, b) => {
-//     console.log(a + b);
-//   });
+console.log("hoolp");
+let calculator =
+  (1,
+  2,
+  (a, b) => {
+    console.log(a + b);
+  });
 
-// calculator();
-// function outerFun() {
-//   function innerFun() {
-//     console.log("hii");
-//   }
-//   innerFun();
-// }
-// function getData(dataId, getNextData) {
-//   return new Promise((resolve,reject) => {
-//     setTimeout(() => {
-//       console.log("data", dataId);
-//       if (getNextData) {
-//         getNextData();
-//       }
-//     }, 2000);
-//   });
-// }
+calculator();
+function outerFun() {
+  function innerFun() {
+    console.log("hii");
+  }
+  innerFun();
+}
+function getData(dataId, getNextData) {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      console.log("data", dataId);
+      if (getNextData) {
+        getNextData();
+      }
+    }, 2000);
+  });
+}
 
-// getData(1, () => {
-//   getData(2, () => {
-//     getData(3, () => {
-//       getData(4);
-//     });
-//   });
-// });
+getData(1, () => {
+  getData(2, () => {
+    getData(3, () => {
+      getData(4);
+    });
+  });
+});
 
 console.log("hey1");
 console.log("hey2");
@@ -50,3 +50,7 @@ setTimeout(() => {
   console.log("hey3");
 }, 2000);
 console.log("hey4");
+
+
+
+
